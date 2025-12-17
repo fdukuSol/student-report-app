f5980215cf5099a62d1dcd43fd2744a15130cf46
fdukuSol <dukefred0707@gmail.com>
2025-12-17 11:22:22 +0000

    Fix st.secrets access syntax

--- core/auth.py
+++ core/auth.py
@@ -3,8 +3,8 @@ from supabase import create_client
 import streamlit as st
 import os
 
-SUPABASE_URL = st.secrets("SUPABASE_URL")
-SUPABASE_KEY = st.secrets("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
--- core/db_subject_order.py
+++ core/db_subject_order.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 load_dotenv()
 
-SUPABASE_URL = st.secrets("SUPABASE_URL")
-SUPABASE_KEY = st.secrets("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
